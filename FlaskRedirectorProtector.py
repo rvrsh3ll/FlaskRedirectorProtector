03a0de26d76028d5f51e0d6128177a620b0e282d
rvrsh3ll <rvrsh3ll@gmail.com>
2020-02-16 13:18:26 -0500

    Update
    Add blacklist and whitelist based on useragent

--- FlaskRedirectorProtector.py
+++ FlaskRedirectorProtector.py
@@ -1,11 +1,11 @@
 
+from flask import Flask, render_template, redirect, make_response, jsonify, request, send_from_directory
 import flask
 import requests
 import argparse
-import logging
-from logging.handlers import RotatingFileHandler
+import os
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 method_requests_mapping = {
     'GET': requests.get,
@@ -17,40 +17,78 @@ method_requests_mapping = {
     'OPTIONS': requests.options,
 }
 
+@app.before_request
+def log_request():
+  # Logging
+    ip = flask.request.remote_addr
+    request = flask.request.base_url
+    ua = flask.request.headers.get('User-Agent')
+    with open("access.log", "a") as log:
+        log.write("ip: " + ip + "," + " User-Agent: " + ua + "," + " Request: " + request)
+        log.write('\n')
 
-@app.route('/<path:path>', methods=method_requests_mapping.keys())
-def proxy(path):
-    url = teamserver + path
-    requests_function = method_requests_mapping[flask.request.method]
-    if headerkey in flask.request.headers.get(header):
-        remote_addr = flask.request.remote_addr
-        base_url = flask.request.base_url
-        app.logger.warning("Secret Cookie Requested from:" + remote_addr + " to " + url )
-        request = requests_function(url, stream=True, params=flask.request.args)
-        response = flask.Response(flask.stream_with_context(request.iter_content()),
-                          content_type=request.headers['content-type'],
-                          status=request.status_code)
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        return response
+
+@app.route("/files/<path:filename>")
+def fileserve(filename):
+    ua = flask.request.headers.get('User-Agent')
+    if useragent_whitelist is not None:
+        if useragent_whitelist in ua:
+            return send_from_directory(directory="files", filename=filename)
+    elif useragent_blacklist is True:
+        with open('blacklist.txt','r') as f:
+            for x in f:
+                x = x.rstrip()
+                if ua == x:
+                    return redirect(redirect_url, code=302)
+                else:
+                    return send_from_directory(directory="files", filename=filename)
     else:
         return redirect(redirect_url, code=302)
+    
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=method_requests_mapping.keys())
+def teamserver(path):
+    if header:
+        if headerkey in flask.request.headers.get(header):
+            url = teamserver + path
+            requests_function = method_requests_mapping[flask.request.method]
+            ip = flask.request.remote_addr
+            request = flask.request.base_url
+            ua = flask.request.headers.get('User-Agent')
+            with open("access.log", "a") as log:
+                log.write("Secret Cookie Requested from: " + ip)
+                log.write('\n')
+            request = requests_function(url, stream=True, params=flask.request.args)
+            response = flask.Response(flask.stream_with_context(request.iter_content()),
+                              content_type=request.headers['content-type'],
+                              status=request.status_code)
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+        else:
+            return redirect(redirect_url, code=302)
+    return redirect(redirect_url, code=302)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Protect your redirector/teamserver")
     parser.add_argument('--teamserver', help="Teamserver to forward to. Example: http://ip:port/", required=False)
-    parser.add_argument('--port', help="Port to listen on", required=True)
-    parser.add_argument('--header', help="Header to allow traffic. Example: 'X-Aspnet-Version'", required=True)
-    parser.add_argument('--headerkey', help="Header key. Example: '1.5'", required=True)
+    parser.add_argument('--host', help="Host to listen on.", default="0.0.0.0", required=True)
+    parser.add_argument('--port', help="Port to listen on.", required=True)
+    parser.add_argument('--header', help="Header to allow traffic. Example: 'X-Aspnet-Version'", required=False)
+    parser.add_argument('--headerkey', help="Header key. Example: '1.5'", required=False)
     parser.add_argument('--redirect_url', help="Redirect URL.", required=True)
+    parser.add_argument('--serve_payloads', action='store_true', help="Switch: Serve Payloads from files folder", required=False)
+    parser.add_argument('--useragent_whitelist', help="Custom Useragent to allow.",required=False)
+    parser.add_argument('--useragent_blacklist', action='store_true', help="Custom Useragent file to disallow.", required=False)
     args = parser.parse_args()
     port = int(args.port)
+    useragent_whitelist = args.useragent_whitelist
+    useragent_blacklist = args.useragent_blacklist
+    serve_payloads = args.serve_payloads
     redirect_url = args.redirect_url
     teamserver = args.teamserver
     headerkey = args.headerkey
     header = args.header
-    handler = RotatingFileHandler('access.log', maxBytes=10000, backupCount=1)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
+    host = args.host
     app.debug = False
-    app.run(host="0.0.0.0",port=port)
+    app.run(host=host,port=port)
--- README.md
+++ README.md
@@ -5,8 +5,17 @@
  The same may be accomplished for standard redirectors as well.
  Reference: https://medium.com/@rvrsh3ll/hardening-your-azure-domain-front-7423b5ab4f64
 
- Example:
+ ## Examples:
+
+ ### Allow traffic using a secret header
 
  python3 FlaskRedirectorProtector.py --port 80 --redirect_url https://www.google.com --teamserver "http://host:80/" --headerkey "1.5" --header "X-Aspnet-Version"
 
  curl -H "X-Aspnet-Version: 1.5" http://redirector/testing
+
+ ### Allow traffic to a payload based on useragent
+ python3 FlaskRedirectorProtector2.py --port 8080 --serve_payloads --redirect_url https://www.google.com --useragent_whitelist "Google" --host "0.0.0.0"
+
+### Disallow traffic to a payload based on useragent in blacklist.txt
+python3 FlaskRedirectorProtector2.py --port 8080 --serve_payloads --redirect_url https://www.google.com --useragent_blacklist - --host "0.0.0.0"
+
